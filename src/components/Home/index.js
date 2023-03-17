64ebb84fd2c243035d024076c703985c884b35d0
Pranjal2803 <pranjalchavan2000@gmail.com>
2023-03-17 07:25:26 +0000

    DebuggingFetchingandRouting

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+
+import './index.css'
+
+const Home = () => {
+  ;<div className="Home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+}
+
+export default Home
